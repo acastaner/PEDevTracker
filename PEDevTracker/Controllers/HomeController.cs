7aaa78372f8fe71499b920e12396f4806a6a53c2
Arnaud Castaner <furism@gmail.com>
2013-09-18 22:23:56 +0200

    Added paging
    HTML is wrong but backend code is working, it seems

--- PEDevTracker/Controllers/HomeController.cs
+++ PEDevTracker/Controllers/HomeController.cs
@@ -12,17 +12,24 @@ namespace PEDevTracker.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             var fluentConfig = HibernateModule.GetConfiguration();
             var s = HibernateModule.CreateSession();
-            IList<Post> latestDevPosts = s.QueryOver<Post>()
-                                .OrderBy(x => x.Date).Desc
-                                .Take(100)
-                                .List<Post>();
 
+            var request = s.QueryOver<Post>()
+                            .OrderBy(x => x.Date).Desc;
+
+            int postCount = request.RowCount();
+
+            var actualPosts = request
+                                .Skip(page.GetValueOrDefault() * 10)
+                                .Take(10)
+                                .List();
+            
+            ViewBag.Pagination = new Pagination(postCount, 10, page, "Index", null);
             ViewBag.Message = "Tracking the developers of Project: Eternity.";
-            return View(latestDevPosts);  
+            return View(actualPosts);  
         }
 
         public ActionResult About()
---
+++ PEDevTracker/Models/Pagination.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEDevTracker.Models
+{
+    public class Pagination
+    {
+        public long NumberOfPages { get; set; }
+        public int PageNumber { get; set; }
+        public string Action { get; set; }
+        public object RouteValues { get; set; }
+
+        public Pagination(long numberOfResults, int resultsPerPage, int? page, string action, object routeValues)
+        {
+            this.NumberOfPages = numberOfResults / resultsPerPage + (numberOfResults % resultsPerPage > 0 ? 1 : 0);
+            this.PageNumber = (page != null ? (int)page : 0);
+            this.Action = action;
+            this.RouteValues = routeValues;
+        }
+    }
+}
